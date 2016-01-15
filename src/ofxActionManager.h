7247176c92c8425fee5a7e89c6eaf3eb8209ee59
Furkan Üzümcü <furkan.uzumcu@edmoware.com>
2016-01-15 17:07:03 +0200

    Add main include file

---
+++ src/ofxActionManager.h
@@ -0,0 +1,46 @@
+#ifndef OFXACTIONMANAGER_H
+#define OFXACTIONMANAGER_H
+#include "ofxActionManager.h"
+#include "TweenFunction.h"
+#include "uthash.h"
+#include "utlist.h"
+#include "Action.h"
+#include "ActionEase.h"
+#include "ActionInstant.h"
+#include "ActionInterval.h"
+#include "ActionManager.h"
+#include "ActionTarget.h"
+#include "Array.h"
+
+//Action Instant
+#include "ActionInstant/CallFunc.h"
+
+//Action Interval
+#include "ActionInterval/BezierBy.h"
+#include "ActionInterval/BezierTo.h"
+#include "ActionInterval/DelayTime.h"
+#include "ActionInterval/MoveBy.h"
+#include "ActionInterval/MoveTo.h"
+#include "ActionInterval/Sequence.h"
+
+//Easings
+#include "ActionInterval/Easings/EaseBack.h"
+#include "ActionInterval/Easings/EaseBezierAction.h"
+#include "ActionInterval/Easings/EaseBounce.h"
+#include "ActionInterval/Easings/EaseCircle.h"
+#include "ActionInterval/Easings/EaseCubic.h"
+#include "ActionInterval/Easings/EaseElastic.h"
+#include "ActionInterval/Easings/EaseExponential.h"
+#include "ActionInterval/Easings/EaseIn.h"
+#include "ActionInterval/Easings/EaseInOut.h"
+#include "ActionInterval/Easings/EaseOut.h"
+#include "ActionInterval/Easings/EaseQuadratic.h"
+#include "ActionInterval/Easings/EaseQuartic.h"
+#include "ActionInterval/Easings/EaseQuintic.h"
+#include "ActionInterval/Easings/EaseSineIn.h"
+#include "ActionInterval/Easings/EaseSineInOut.h"
+#include "ActionInterval/Easings/EaseSineOut.h"
+
+
+#endif // OFXACTIONMANAGER_H
+
